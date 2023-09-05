c6127806a02c88f63a2515d30852e8d4b32465c3
Eliot Valdes <eliottvaldes@hotmail.com>
2023-09-05 16:58:04 -0600

    PDI: apuntes de uso basico de matlab

---
+++ PDI/P1/Apuntes/01-lectura-de-imagen.m
@@ -0,0 +1,19 @@
+whos
+a = [1,5,7,4,3]
+
+% obtencion de un valor en especifico
+a(1,4) // imprime el valor 
+
+// definicion de un arreglo
+b = [1,2,3; 6,9,12; 4,5,6]
+
+// obtencion de columnas completas
+b(:2,:3) // obtencion de las columnas 2 y 3
+
+// obtencion de filas completas despues de la 2 hasta el final
+b(2:,:end)
+
+// obtencion de fila central 
+b(2,:)
+
+
